1fdcddfb7bafec8227078f0722454bdf78446faf
morajendran <r.mohandhasgandhi@outlook.com>
2024-05-24 16:46:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ if ingredient_list:
     
     for fruit_chosen in ingredient_list:
         ingredient_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     #st.write(ingredient_string)
 
@@ -39,6 +41,4 @@ if ingredient_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
